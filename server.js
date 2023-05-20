7fe71c26b9a79c3d9b743d8a4bd72186b39dc86e
shantanu-bwii <shantanubwii76@gmail.com>
2023-05-20 16:49:02 +0530

    access commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 5000;
 const app = express();
 
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'https://bs-staking.netlify.app', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
